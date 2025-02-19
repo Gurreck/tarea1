33810bb7aef9043d96c8852e6847b727a5359923
Gurreck <69431789+Gurreck@users.noreply.github.com>
2025-02-18 21:11:49 -0600

    Connection to Mixnet1 Fixed

--- connectionMixnet.py
+++ connectionMixnet.py
@@ -2,12 +2,12 @@ import socket
 import struct
 import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives import hashes, serialization
-from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes, serialization, padding
+from cryptography.hazmat.primitives.asymmetric import padding as async_padding
 
 # Configuración de la conexión
 MIXNET_HOST = "pets.ic-itcr.ac.cr"
-MIXNET_PORT = 50027  # Puerto ficticio, actualizar con el real
+MIXNET_PORT = 50028  # Puerto ficticio, actualizar con el real
 
 # Cargar clave pública desde un archivo PEM
 def load_public_key(filename):
@@ -22,18 +22,18 @@ def generate_aes_key():
 
 # Cifrado AES con PKCS7 padding
 def encrypt_aes(key, iv, plaintext):
-    pad_length = 16 - (len(plaintext) % 16)
-    plaintext += bytes([pad_length] * pad_length)
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
     encryptor = cipher.encryptor()
+    padder = padding.PKCS7(128).padder()
+    plaintext = padder.update(plaintext) + padder.finalize()
     return encryptor.update(plaintext) + encryptor.finalize()
 
 # Cifrar clave AES e IV con RSA
-def encrypt_rsa(public_key, key, iv):
+def encrypt_rsa(public_key, data):
     return public_key.encrypt(
-        key + iv,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA1()),
+        data,
+        async_padding.OAEP(
+            mgf=async_padding.MGF1(algorithm=hashes.SHA1()),
             algorithm=hashes.SHA1(),
             label=None
         )
@@ -46,7 +46,7 @@ def onion_encrypt(dest, message, keys):
     for key in reversed(keys):  # Cifrar en orden mix-3 -> mix-2 -> mix-1
         aes_key, iv = generate_aes_key()
         plaintext = encrypt_aes(aes_key, iv, plaintext)
-        plaintext = encrypt_rsa(key, aes_key, iv) + plaintext
+        plaintext = encrypt_rsa(key, iv+aes_key) + plaintext
     
     return plaintext
 
@@ -72,5 +72,6 @@ if __name__ == "__main__":
         load_public_key("public-key-mix-1.pem"),
         load_public_key("public-key-mix-2.pem"),
         load_public_key("public-key-mix-3.pem")
+
     ]
     send_message("Estudiante3", "Grupo 3", keys)
